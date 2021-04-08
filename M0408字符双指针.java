8ee36677e2c0a4feb95b3c2c7f72c040bc5137f0
houh <54264174+houhwyq@users.noreply.github.com>
2021-04-08 20:11:26 +0800

    Update M0408字符双指针.java

--- M0408字符双指针.java
+++ M0408字符双指针.java
@@ -27,3 +27,29 @@ public class E0408字符双指针 {
         }
     }
 }
+//改进版本
+class Solution {
+    public int minimumLength(String s) {
+        char ans[]=s.toCharArray();
+        int len=s.length();
+        int left=0;int right=len-1;
+            while(left<right){
+                char a=ans[right],b=ans[left];
+                if(a==b){
+                if(left+1==right)return 0;
+                else{
+                    while(a==ans[left]&&left<right){
+                        left++;
+                        if(left==right)return 0;
+                    }
+                    while(b==ans[right]&&left<right){
+                        right--;
+                         if(left==right)return 1;
+                    
+                }
+            }
+        }else break;
+        }
+       return right-left+1;   
+    }
+}
